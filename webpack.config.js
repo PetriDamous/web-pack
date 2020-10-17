518dad2263a6761ef654f48b8194bff6f785a07d
dwilliams64 <dwilliams_64@yahoo.com>
2020-10-17 15:00:45 -0500

    1st commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist/JS")
+    },
+    resolve: {
+        extensions: [
+            ".js"
+        ]
+    }
+};
